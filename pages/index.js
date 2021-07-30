64aa5f63b8140e7af4a43ee06635071215567b3d
Emerick <gulhermeemerick@gmail.com>
2021-07-30 17:22:04 -0300

    Pequenos ajustes na página principal.

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,29 @@ export default function Home() {
       <div className={styles.menu}>
         <a href="/">.me( )</a>
         <div className={styles.treeitems}>
-        <a href="/" className={styles.items}>.about( )</a>
-        <a href="/" className={styles.items}>.work( )</a>
-        <a href="/" className={styles.items}>.contact( )</a>
-        <a href="/" className={styles.items}>.experience( )</a>
+          <a href="/" className={styles.items}>
+            .about( )
+          </a>
+          <a href="/" className={styles.items}>
+            .work( )
+          </a>
+          <a href="/" className={styles.items}>
+            .contact( )
+          </a>
+          <a href="/" className={styles.items}>
+            .experience( )
+          </a>
+        </div>
       </div>
-      </div>      
+      <div className={styles.titulo}>WebDeveloper</div>
+      <div className={styles.corpo}>
+      <div>socials icons</div>
+        <div className={styles.corpoBox}>
+          <div>Text</div>
+          <div>Avatar</div>
+          <div>About</div>
+        </div>
+      </div>
     </section>
   );
 }
