9bcedc503b13cc07ce8d922677124a1c384f014a
Emerick <gulhermeemerick@gmail.com>
2021-07-30 19:20:38 -0300

    Ajustes
    Foto + textos

--- pages/index.js
+++ pages/index.js
@@ -38,9 +38,28 @@ export default function Home() {
           </a>
         </div>
         <div className={styles.corpoBox}>
-          <div>About me</div>
-          <div>Avatar</div>
-          <div>Details</div>
+          <div className={styles.corpoItens}>
+            <h1 className={styles.corpoTitle}>About Me</h1>
+            <h3 className={styles.corpoText}>
+              I am a FrontEnd developer, graduated at Brazil wich have 2 yeard
+              experience. I have a passion with my job and i love to learn new
+              technologys and everything about IT area.
+            </h3>
+          </div>
+
+          <div className={styles.corpoItens}>
+            <img src="/me.jpg" alt="" className={styles.me} />
+          </div>
+          <div className={styles.corpoItens}>
+            <h1 className={styles.corpoTitle}>Details</h1>
+            <h3 className={styles.corpoText}>
+              <h5 className={styles.corpoText}>
+                Name: Guilherme Emerick<br></br>
+                Age: 24<br></br>
+                Location: Brasília, Brazil<br></br>
+              </h5>
+            </h3>
+          </div>
         </div>
       </div>
     </section>
